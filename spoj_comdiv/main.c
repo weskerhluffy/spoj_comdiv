64de4c78333db4905b059e57a58885834ebffa90
ernesto.alvarado <ernesto.alvarado@adroll.com>
2019-12-14 07:24:41 -0600

    Initial Commit

---
+++ spoj_comdiv/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  spoj_comdiv
+//
+//  Created by ernesto alvarado on 14/12/19.
+//  Copyright © 2019 ernesto alvarado. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
